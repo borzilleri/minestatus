65e39ed965e4dee01a3f0c40dd4002cb76984ee9
Jonathan Borzilleri <jonathan@borzilleri.net>
2014-08-12 21:47:07 -0700

    Adds RCon library, implements server user list check.
    Nab the RCon code from https://code.google.com/p/rcon-client/, though this is a
    temprary solution, as I'd like to rewrite this in a more async/reactive manner
    if possible.
    
    Handle retrieving server status & online list, rendering the page appropriately.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,25 @@
 package controllers
 
-import play.api._
+import io.rampant.minecraft.rcon.RCon
+import play.api.Play.current
 import play.api.mvc._
 
-object Application extends Controller {
+import scala.collection.JavaConverters._
+import scala.util.Try
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+object Application extends Controller {
+	val rconHost = current.configuration.getString("minecraft.rcon.host").get
+	val rconPort = current.configuration.getInt("minecraft.rcon.port").get
+	val rconPass = current.configuration.getString("minecraft.rcon.password").get
+
+	def index = Action {
+		val users = Try(new RCon(rconHost, rconPort, rconPass))
+			.flatMap({ c => Try(c.list().asScala.toList)})
+		Ok(views.html.index(users.isSuccess, users.getOrElse(List())))
+	}
+
+	def start = Action {
+		Redirect(routes.Application.index)
+	}
 
 }
--- build.sbt
+++ build.sbt
@@ -1,14 +1,19 @@
 name := """minecraft-rcon"""
 
+organization := "io.rampant.minecraft"
+
 version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.1"
 
+resolvers += "spray repo" at "http://repo.spray.io"
+
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache,
-  ws
+	"org.webjars" % "bootstrap" % "3.2.0",
+	"com.decodified" %% "scala-ssh" % "0.6.4",
+	"org.bouncycastle" % "bcprov-jdk16" % "1.46",
+	"com.jcraft" % "jzlib" % "1.1.3"
 )
+
