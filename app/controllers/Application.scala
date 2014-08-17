58f4da414c8a0ec72b68e63ba357e2f96c61a71a
Jonathan Borzilleri <jonathan@borzilleri.net>
2014-08-16 20:16:05 -0700

    Implements multi-server capability.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,31 +1,45 @@
 package controllers
 
 import com.decodified.scalassh.SSH
+import com.typesafe.config.ConfigFactory
 import io.rampant.minecraft.rcon.RCon
+import play.api.Configuration
 import play.api.Play.current
 import play.api.mvc._
 
 import scala.collection.JavaConverters._
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object Application extends Controller {
-	val host = current.configuration.getString("minecraft.host").get
-	val rconPort = current.configuration.getInt("minecraft.rcon.port").get
-	val rconPass = current.configuration.getString("minecraft.rcon.password").get
-	val startCmd = current.configuration.getString("minecraft.command").get
+
+	case class MCServer(id: String, name: String, running: Boolean, players: List[String])
+
+	val rconDefaults = current.configuration.getConfig("server.defaults").get.underlying
+	val configuredServers = ConfigFactory.load().getConfigList("servers").asScala.map({ c => Configuration(c.withFallback(rconDefaults))})
 
 	def index = Action {
-		val users = Try(new RCon(host, rconPort, rconPass))
-			.flatMap({ c => Try(c.list().asScala.toList)})
-		Ok(views.html.index(users.isSuccess, users.getOrElse(List())))
+		val servers = configuredServers.map({ s =>
+			val users = Try(new RCon(s.getString("host").get, s.getInt("rcon.port").get, s.getString("rcon.password").get))
+				.flatMap({ c => Try(c.list().asScala.toList)})
+			users match {
+				case Success(v) => ;
+				case Failure(e) => play.api.Logger.error("rcon error", e)
+			}
+			MCServer(s.getString("id").get, s.getString("name").get, users.isSuccess, users.getOrElse(List()))
+		})
+		Ok(views.html.index(servers))
 	}
 
-	def start = Action {
-		SSH(host) { client =>
-			client.exec(startCmd).right.map { result =>
-				play.api.Logger.debug(result.stdOutAsString());
-			}
+	def start(id: String) = Action {
+		configuredServers.find(_.getString("id").get.equalsIgnoreCase(id)) match {
+			case None => NotFound("Unknown server: " + id)
+			case Some(s) =>
+				SSH(s.getString("host").get) { client =>
+					client.exec(s.getString("command").get).right.map { result =>
+						play.api.Logger.debug(result.stdOutAsString());
+					}
+				}
+				Redirect(routes.Application.index)
 		}
-		Redirect(routes.Application.index)
 	}
 }
